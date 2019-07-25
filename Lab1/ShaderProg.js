db9c4b13bf770665037c4725fc15d1a6da46a8ad
studemark <studemark@gmail.com>
2019-07-25 11:17:06 -0500

    Added Cube.js and modified a few things.

--- Lab1/ShaderProg.js
+++ Lab1/ShaderProg.js
@@ -1,33 +1,37 @@
 function makeShaderProgram(gl, vsSource, fsSource, attNames, ufmNames) {
 
-   const program = {
-      program: shaderProgram,
-      attLocs: {
-        vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
-        vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
-      },
-      ufmLocs: {
-        projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
-        modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
-      },
-    };
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
-   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-   const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
- 
-   // Create the shader program
- 
-   const shaderProgram = gl.createProgram();
-   gl.attachShader(shaderProgram, vertexShader);
-   gl.attachShader(shaderProgram, fragmentShader);
-   gl.linkProgram(shaderProgram);
- 
-   // If creating the shader program failed, alert
- 
-   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-     console.log('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
-     return null;
-   }
- 
-   return program;
+  // Create the shader program
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  // If creating the shader program failed, alert
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    console.log('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    return null;
+  }
+
+  var attLocs = {};
+  attNames.forEach(function(name){
+      attLocs[name] = gl.getAttribLocation(shaderProgram, name);
+  });
+
+  var ufmLocs = {};
+  ufmNames.forEach(function(name){
+      ufmLocs[name] = gl.getUniformLocation(shaderProgram, name);
+  });
+
+  const programInfo = {
+    program: shaderProgram,
+    attLocs,
+    ufmLocs,
+  };
+  
+   return programInfo;
  }
--- Sandbox/TestGLM.js
+++ Sandbox/TestGLM.js
@@ -1,9 +1,22 @@
 main();
 
 function main() {
-   var lat = Math.PI/3, lng = Math.PI/5;
+   /* var lat = 38.948 * (Math.PI/180), lng = -90.351 * (Math.PI/180); //Degrees converted to rads and lng is negative because we're moving west
 
-   console.log(mat4.create());
-   var latRotate = mat4.fromZRotation(mat4.create(), lat);
-   console.log(latRotate);
+   console.log(vec4.transformMat4(vec4.create(), [6500, 0, 0, 1], mat4.multiply(mat4.create(), mat4.rotateY(mat4.create(), mat4.create(), lng), mat4.rotateZ(mat4.create(), mat4.create(), lat)))); */
+
+   console.log(vec4.scale([], [0, 1, 0, 1], 3));
+   console.log(vec4.scale([], [0, -1, 0, 1], 3));
+   console.log(vec4.transformMat4([], vec4.scale([], [0, 1, 0, 1], 3),  mat4.rotateZ(mat4.create(), mat4.create(), Math.PI/2)));
+   console.log(vec4.transformMat4([], vec4.scale([], [0, -1, 0, 1], 3), mat4.rotateZ(mat4.create(), mat4.create(), Math.PI/2)));
+
+   const xfm = mat4.create();
+   mat4.translate(xfm, xfm, [0, 0, -2]);
+   mat4.rotateZ(xfm, xfm, -Math.PI/2);
+   mat4.scale(xfm, xfm, [1, 3, 1]);
+
+   var north = [0, 1, 0, 1];
+   var south = [0, -1, 0, 1];
+
+   console.log(vec4.transformMat4(north, north, xfm), vec4.transformMat4(south, south, xfm));
 }
