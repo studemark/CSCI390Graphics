03ed7281a43a9b07aa378b38c43e0e12e54291c3
studemark <studemark@gmail.com>
2019-08-01 13:13:40 -0500

    Started work on camera rotation, renamed webgl-demo to Main.

--- Lab2/Main.js
+++ Lab2/Main.js
@@ -43,45 +43,76 @@ function main() {
 
    const programInfo = makeShaderProgram(gl, vsSource, fsSource, ['positions', 'color'], ['projectionMatrix', 'viewMatrix']);
 
-   // Collect all the info needed to use the shader program.
-   // Look up which attributes our shader program is using
-   // for positions, aVevrtexColor and also
-   // look up uniform locations.
-
+   const mvMatrix = mat4.create();
+   
    // Here's where we call the routine that builds all the
    // objects we'll be drawing.
    //const cube = new CubeModel(gl);
    const jack = new JackStackAttack(gl);
-
+   
    // Draw the scene repeatedly
    function doFrame(now) {
       now *= 0.001;  // convert to seconds
-    
-      drawScene(gl, programInfo, jack, now);
-    
+      
+      drawScene(gl, programInfo, jack, now, mvMatrix);
+      
       requestAnimationFrame(doFrame);
    }
    requestAnimationFrame(doFrame);
+   
+   var r = -10;
+   
+   var lat = 0;
+   var lng = 0;
+   
+   var x = r * Math.cos(lat) * Math.cos(lng);
+   var y = r * Math.sin(lat);
+   var z = -(r * Math.cos(lat) * Math.sin(lng));
+   
+   mat4.translate(mvMatrix, mvMatrix, [x, y, z]);
+   console.log(mvMatrix);
+   console.log(x);
+   console.log(y);
+   console.log(z);
+
+   document.addEventListener("keydown", event => {
+      if (event.code === "ArrowDown" ) {
+
+         mat4.rotate(mvMatrix, mvMatrix, Math.PI/10, [x, y, z]);
+      }
+   
+      else if (event.code === "ArrowUp" ) {
+         mat4.rotate(mvMatrix, mvMatrix, Math.PI/10, [x, y, z]);
+      }
+   
+      else if (event.code === "ArrowLeft" ) {
+         mat4.rotate(mvMatrix, mvMatrix, Math.PI/10, [x, y, z]);
+      }
+   
+      else if (event.code === "ArrowRight" ) {
+         mat4.rotate(mvMatrix, mvMatrix, Math.PI/10, [x, y, z]);
+      }
+
+      else if (event.code === "KeyF") {
+         mat4.translate(mvMatrix, mvMatrix, [x, y, z])
+      }
+
+      else if (event.code === "KeyG") {
+         mat4.translate(mvMatrix, mvMatrix, [x, y, z])
+      }
+      
+   });
+
 } 
-//
-// Draw the scene.
-//
-function drawScene(gl, programInfo, jack, time) {
+
+function drawScene(gl, programInfo, jack, time, mvMatrix) {
    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
    gl.clearDepth(1.0);                 // Clear everything
    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
 
-   // Clear the canvas before we start drawing on it.
 
    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-   // Create a perspective matrix, a special matrix that is
-   // used to simulate the distortion of perspective in a camera.
-   // Our field of view is 45 degrees, with a width/height
-   // ratio that matches the display size of the canvas
-   // and we only want to see objects between 0.1 units
-   // and 100 units away from the camera.
 
    const fieldOfView = 45 * Math.PI / 180;   // in radians
    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
@@ -89,28 +120,19 @@ function drawScene(gl, programInfo, jack, time) {
    const zFar = 100.0;
    const projectionMatrix = mat4.create();
 
-   // note: glmatrix.js always has the first argument
-   // as the destination to receive the result.
    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
 
-   // Set the drawing position to the "identity" point, which is
-   // the center of the scene.
-   const modelViewMatrix = mat4.create();
-
-   // Now move the drawing position a bit to where we want to
-   // start drawing the square.
-
-   mat4.translate(modelViewMatrix,     // destination matrix
-    modelViewMatrix,     // matrix to translate
-    [0.0, 0.0, -6.0]);  // amount to translate
-   mat4.rotate(modelViewMatrix,  // destination matrix
-    modelViewMatrix,  // matrix to rotate
+   /* mat4.translate(mvMatrix,     // destination matrix
+    mvMatrix,     // matrix to translate
+    [0.0, 0.0, -10.0]);  // amount to translate */
+   /* mat4.rotate(mvMatrix,  // destination matrix
+    mvMatrix,  // matrix to rotate
     cubeRotation * Math.PI / 4,     // amount to rotate in radians
     [0, 0, 1]);       // axis to rotate around (Z)
-   mat4.rotate(modelViewMatrix,  // destination matrix
-    modelViewMatrix,  // matrix to rotate
+   mat4.rotate(mvMatrix,  // destination matrix
+    mvMatrix,  // matrix to rotate
     cubeRotation * Math.PI / 4,// amount to rotate in radians
-    [1, 1, 0]);       // axis to rotate around (Y)
+    [1, 1, 0]);       // axis to rotate around (Y) */
    
    gl.useProgram(programInfo.program);
 
@@ -119,13 +141,9 @@ function drawScene(gl, programInfo, jack, time) {
     false,
     projectionMatrix); 
 
-   // Tell WebGL how to pull out the positions from the position
-   // buffer into the vertexPosition attribute
-   //cube.render(gl, programInfo, modelViewMatrix);
-   jack.render(gl, programInfo, modelViewMatrix);
-   // Update the rotation for the next draw
+   jack.render(gl, programInfo, mvMatrix);
 
-   cubeRotation = time;
+   //cubeRotation = time;
 }
 
 function loadShader(gl, type, source) {
