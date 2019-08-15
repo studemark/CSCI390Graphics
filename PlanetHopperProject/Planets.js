eb2db7810098e96f30a5afc82c1a58b760b01a43
EliHasselbring <elihasselbring@hotmail.com>
2019-08-15 10:46:40 -0500

    Started working on Planets and World generation.

---
+++ PlanetHopperProject/Planets.js
@@ -0,0 +1,16 @@
+class Planets {
+    constructor() {
+        var cylinder;
+        var sphere;
+        var box;
+        var cone;
+        var dodecahedron;
+        var icosehedron;
+        var lathe;
+        var octahedron;
+        var tetrahedron;
+        var torus;
+        var torusKnot;
+        var tube;
+    }
+}
