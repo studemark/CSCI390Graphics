105fd7edfb37911e92f4b28d6e7c6d33f0112b68
studemark <studemark@gmail.com>
2019-08-15 11:51:53 -0500

    Finished planet generation.

--- PlanetHopperProject/Planets.js
+++ PlanetHopperProject/Planets.js
@@ -1,16 +1,93 @@
 class Planets {
-    constructor() {
-        var box;
-        var cone;
-        var cylinder;
-        var dodecahedron;
-        var icosehedron;
-        var lathe;
-        var octahedron;
-        var sphere;
-        var tetrahedron;
-        var torus;
-        var torusKnot;
-        var tube;
-    }
+   constructor() {}
+
+   makePlanet() {
+      const functionList = [
+         function() {
+            var geometry = new THREE.BoxGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.ConeGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.CylinderGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.DodecahedronGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.IcosahedronGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.LatheGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.OctahedronGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.SphereGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.TetrahedronGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.TorusGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.TorusKnotGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         },
+
+         function() {
+            var geometry = new THREE.TubeGeometry();
+            var material = new THREE.MeshBasicMaterial(
+             {color: (Math.random() * 0xFFFFFF << 0).toString(16)});
+            return new THREE.Mesh(geometry, material);
+         }
+      ];
+
+      return functionList[Math.floor(Math.random() * Math.floor(12))];
+   }
 }
