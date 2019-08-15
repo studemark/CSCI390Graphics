4d50a6eac54f75ab5383889aaa43857fefb65ae0
studemark <studemark@gmail.com>
2019-08-15 10:50:19 -0500

    Working on Main and Planets.

---
+++ PlanetHopperProject/Main.js
@@ -0,0 +1,18 @@
+function main() {
+   var renderer =  new THREE.WebGLRenderer({antialias: true});
+   renderer.setSize(window.innerWidth, window.innerHeight);
+   document.body.appendChild(renderer.domElement);
+
+   const ambLight = new THREE.AmbientLight(0x808080);
+   const pntLight = new THREE.PointLight(0xFFFFFF, .7);
+   pntLight.position.set(5, 5, 10);
+
+   var camera = new THREE.PerspectiveCamera(30, 
+    window.innerWidth/window.innerHeight, 0.1, 1000);
+   camera.position.z = 5;
+
+   var scene = new THREE.Scene();
+   scene.add(ambient);
+   scene.add(pntLight);
+   
+}
--- PlanetHopperProject/Planets.js
+++ PlanetHopperProject/Planets.js
@@ -1,13 +1,13 @@
 class Planets {
     constructor() {
-        var cylinder;
-        var sphere;
         var box;
         var cone;
+        var cylinder;
         var dodecahedron;
         var icosehedron;
         var lathe;
         var octahedron;
+        var sphere;
         var tetrahedron;
         var torus;
         var torusKnot;
