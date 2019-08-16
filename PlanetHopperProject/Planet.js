913d025bf0125ba90c99c3159b61fa6a4dad418f
studemark <studemark@gmail.com>
2019-08-16 10:43:27 -0500

    Stripped down Planet.

--- PlanetHopperProject/Planet.js
+++ PlanetHopperProject/Planet.js
@@ -1,140 +1,48 @@
-class Planet extends THREE.Mesh {
-   constructor() { //8, 3, 5
-
-      function scaleRand() {
-         return (Math.random() * (2 - 0.5) + 0.5);
-      }
-
-      function getRand(min, max) {
-         var diff = max - (min);
-         return Math.floor((Math.random()) * diff - (max));
-      }
-
+class Planet {
+   constructor() {
       const functionList = [
          function() {
-            var geometry = new THREE.BoxGeometry().scale(scaleRand(), 
-             scaleRand(), scaleRand()).translate(getRand(-8, 8), 
-             getRand(-3, 3), getRand(-5, 5));
-            console.log(scaleRand());
-            var material = new THREE.MeshPhongMaterial(
-             {color: (Math.random() * 0xFFFFFF)});
-            return {
-               geo: geometry, 
-               mat: material
-            }
+            return new THREE.BoxGeometry();
          },
 
          function() {
-            var geometry = new THREE.ConeGeometry().scale(scaleRand(), 
-            scaleRand(), scaleRand()).translate(getRand(-8, 8), 
-             getRand(-3, 3), getRand(-5, 5));
-            var material = new THREE.MeshPhongMaterial(
-             {color: (Math.random() * 0xFFFFFF)});
-            return {
-               geo: geometry, 
-               mat: material
-            }
+            return new THREE.ConeGeometry();
          },
 
          function() {
-            var geometry = new THREE.CylinderGeometry().scale(scaleRand(), 
-            scaleRand(), scaleRand()).translate(getRand(-8, 8), 
-             getRand(-3, 3), getRand(-5, 5));
-            var material = new THREE.MeshPhongMaterial(
-             {color: (Math.random() * 0xFFFFFF)});
-            return {
-               geo: geometry, 
-               mat: material
-            }
+            return new THREE.CylinderGeometry();
          },
 
          function() {
-            var geometry = new THREE.DodecahedronGeometry().scale(scaleRand(), 
-            scaleRand(), scaleRand()).translate(getRand(-8, 8), 
-             getRand(-3, 3), getRand(-5, 5));
-            var material = new THREE.MeshPhongMaterial(
-             {color: (Math.random() * 0xFFFFFF)});
-            return {
-               geo: geometry, 
-               mat: material
-            }
+            return new THREE.DodecahedronGeometry();
          },
 
          function() {
-            var geometry = new THREE.IcosahedronGeometry().scale(scaleRand(), 
-            scaleRand(), scaleRand()).translate(getRand(-8, 8), 
-             getRand(-3, 3), getRand(-5, 5));
-            var material = new THREE.MeshPhongMaterial(
-             {color: (Math.random() * 0xFFFFFF)});
-            return {
-               geo: geometry, 
-               mat: material
-            }
+            return new THREE.IcosahedronGeometry();
          },
 
          function() {
-            var geometry = new THREE.OctahedronGeometry().scale(scaleRand(), 
-            scaleRand(), scaleRand()).translate(getRand(-8, 8), 
-             getRand(-3, 3), getRand(-5, 5));
-            var material = new THREE.MeshPhongMaterial(
-             {color: (Math.random() * 0xFFFFFF)});
-            return {
-               geo: geometry, 
-               mat: material
-            }
+            return new THREE.OctahedronGeometry();
          },
 
          function() {
-            var geometry = new THREE.SphereGeometry().scale(scaleRand(), 
-            scaleRand(), scaleRand()).translate(getRand(-8, 8), 
-             getRand(-3, 3), getRand(-5, 5));
-            var material = new THREE.MeshPhongMaterial(
-             {color: (Math.random() * 0xFFFFFF)});
-            return {
-               geo: geometry, 
-               mat: material
-            }
+            return new THREE.SphereGeometry();
          },
 
          function() {
-            var geometry = new THREE.TetrahedronGeometry().scale(scaleRand(), 
-            scaleRand(), scaleRand()).translate(getRand(-8, 8), 
-             getRand(-3, 3), getRand(-5, 5));
-            var material = new THREE.MeshPhongMaterial(
-             {color: (Math.random() * 0xFFFFFF)});
-            return {
-               geo: geometry, 
-               mat: material
-            }
+            return new THREE.TetrahedronGeometry();
          },
 
          function() {
-            var geometry = new THREE.TorusGeometry().scale(scaleRand(), 
-            scaleRand(), scaleRand()).translate(getRand(-8, 8), 
-             getRand(-3, 3), getRand(-5, 5));
-            var material = new THREE.MeshPhongMaterial(
-             {color: (Math.random() * 0xFFFFFF)});
-            return {
-               geo: geometry, 
-               mat: material
-            }
+            return new THREE.TorusGeometry();
          },
 
          function() {
-            var geometry = new THREE.TorusKnotGeometry().scale(scaleRand(), 
-            scaleRand(), scaleRand()).translate(getRand(-8, 8), 
-             getRand(-3, 3), getRand(-5, 5));
-            var material = new THREE.MeshPhongMaterial(
-             {color: (Math.random() * 0xFFFFFF)});
-            return {
-               geo: geometry, 
-               mat: material
-            }
+            return new THREE.TorusKnotGeometry();
          }
       ];
 
       var newShape = functionList[Math.floor(Math.random() * Math.floor(10))]();
-      console.log(newShape);
-      super(newShape.geo, newShape.mat);
+      this.geometry = newShape;
    }
 }
